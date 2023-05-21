96544f38631e4b2be9002d322fe8cfdd168fc90d
Xalgord <xalgord@gmail.com>
2023-05-21 14:11:04 +0530

    Update mass-param.sh

--- mass-param.sh
+++ mass-param.sh
@@ -3,9 +3,6 @@
 # Path to the ParamSpider script
 PARAMSPIDER_PATH="paramspider.py"
 
-# Path to the httprobe tool
-HTTPROBE_PATH="httprobe"
-
 # Input file containing the list of domains
 DOMAINS_FILE="domains.txt"
 
@@ -15,15 +12,11 @@ OUTPUT_DIR="output"
 # Output file for storing the combined results
 COMBINED_OUTPUT_FILE="combined_output.txt"
 
-# Function to run httprobe on a list of domains and remove dead URLs
-run_httprobe() {
-  input_file="$1"
-  output_file="${input_file}_live.txt"
-
-  echo "Running httprobe on $input_file..."
-  cat "$input_file" | "$HTTPROBE_PATH" | awk -F/ '{print $3}' | sort -u > "$output_file"
-  echo "httprobe completed for $input_file. Live URLs saved in $output_file"
-}
+# Color codes
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m' # No Color
 
 # Function to run ParamSpider on a single domain
 run_paramspider() {
@@ -35,20 +28,20 @@ run_paramspider() {
   output_dir="$OUTPUT_DIR/$domain"
   mkdir -p "$output_dir"
 
-  echo "Running ParamSpider on $domain..."
-  python3 "$PARAMSPIDER_PATH" -d "$domain" --level high -o "$output_dir/output.txt"
-  echo "ParamSpider completed for $domain. Output saved in $output_dir"
+  echo -e "${YELLOW}Running ParamSpider on $domain...${NC}"
+  "$PARAMSPIDER_PATH" -d "$domain" --level high -o "$output_dir/output.txt"
+  if [ -s "$output_dir/output.txt" ]; then
+    echo -e "${GREEN}ParamSpider completed for $domain. Output saved in $output_dir${NC}"
+  else
+    echo -e "${RED}No output file generated for $domain.${NC}"
+  fi
 }
 
-# Read the domain list file and run httprobe to remove dead URLs
-run_httprobe "$DOMAINS_FILE"
-LIVE_DOMAINS_FILE="${DOMAINS_FILE}_live.txt"
-
-# Read the live domains file line by line and run ParamSpider for each domain
+# Read the domains file line by line and run ParamSpider for each domain
 while IFS= read -r domain || [[ -n "$domain" ]]; do
   run_paramspider "$domain"
-done < "$LIVE_DOMAINS_FILE"
+done < "$DOMAINS_FILE"
 
 # Combine the individual domain outputs into a single file
 find "$OUTPUT_DIR" -name "output.txt" -exec cat {} + > "$COMBINED_OUTPUT_FILE"
-echo "Combined output saved in $COMBINED_OUTPUT_FILE"
+echo -e "${GREEN}Combined output saved in $COMBINED_OUTPUT_FILE${NC}"
